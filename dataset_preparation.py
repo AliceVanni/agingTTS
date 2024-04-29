134a73dcbe1ae62e48bce9395b27ceed3cfbe262
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-04-29 18:57:49 +0200

    Update dataset_preparation.py
    printing statements added

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -11,7 +11,7 @@ output_dir = 'CV_17_Delta_prepared'
 
 selected_utterances = select_utterances_from_cv(all_utterances_file)
 renamed_speakers_file = rename_speaker_id(selected_utterances)       
-new_age_group_file = age_group_redefinition(renamed_speakers_file)
+new_age_group_file = age_group_redefinition('selected_validated_sp_renamed.txt')
 
 # Printing the number of speakers and utterances per age group
 age_utterances_df, age_speakers_df = count_speaker_per_group(new_age_group_file, 'age')
@@ -24,8 +24,18 @@ print(f'Utterances per gender\n{gender_utterances_df}')
 print(f'\nSpeakers per gender\n{gender_speakers_df}')
 
 # Generating the file with the dataset with utterances balanced on gender
-#  and age groups
-balanced_dataset_file = select_balanced_utterances(new_age_group_file, 'CV_17_Delta')
+# and age groups
+balanced_dataset_file = select_balanced_utterances(new_age_group_file, 'CV_')
+
+# Printing the number of speakers and utterances per age group of the balanecd dataset
+age_utterances_df, age_speakers_df = count_speaker_per_group(balanced_dataset_file, 'age')
+print(f'Utterances per age group\n{age_utterances_df}')
+print(f'\nSpeakers per age group\n{age_speakers_df}')
+
+# Printing the number of speakers and utterances per gender of the balanecd dataset
+gender_utterances_df, gender_speakers_df = count_speaker_per_group(balanced_dataset_file, 'gender')
+print(f'Utterances per gender\n{gender_utterances_df}')
+print(f'\nSpeakers per gender\n{gender_speakers_df}')
 
 # Creation of the directory with the selected files
 create_dataset(balanced_dataset_file, input_dir, output_dir)
