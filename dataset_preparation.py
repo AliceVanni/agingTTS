1b3aa9134a8a2021d4f45ecc8ab75d0e432efc74
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-01 10:56:13 +0200

    Update dataset_preparation.py
    - Additional print statements added
    - Creation of temp directory to extract the duration of selected files

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -5,10 +5,11 @@ information about the chosen data, such as the number of utterances per age grou
 
 from utils.dataset import select_utterances_from_cv, rename_speaker_id, create_dataset, count_speaker_per_group, age_group_redefinition, select_balanced_utterances, create_file_list, get_audio_folder_duration
 
-all_utterances_file = 'validated_cv_en_17.tsv'
-input_dir = 'cv-corpus-17.0-delta-2024-03-15/en/clips'
-output_dir = 'CV_17_Delta_prepared'
-dataset_name = 'CV_'
+all_utterances_file = 'C:/Users/Alice/cv-corpus-17.0-delta-2024-03-15-en.tar/cv-corpus-17.0-delta-2024-03-15/en/validated.tsv'
+input_dir = 'C:/Users/Alice/cv-corpus-17.0-delta-2024-03-15-en.tar/cv-corpus-17.0-delta-2024-03-15/en/clips'
+output_dir_1 = 'CV_17_prepared_unbalanced'
+output_dir_2 = 'CV_17_prepared_balanced'
+dataset_name = 'CV_17'
 
 selected_utterances = select_utterances_from_cv(all_utterances_file)
 renamed_speakers_file = rename_speaker_id(selected_utterances)       
@@ -23,6 +24,12 @@ print(f'\nSpeakers per age group\n{age_speakers_df}')
 gender_utterances_df, gender_speakers_df = count_speaker_per_group(new_age_group_file, 'gender')
 print(f'Utterances per gender\n{gender_utterances_df}')
 print(f'\nSpeakers per gender\n{gender_speakers_df}')
+
+# Provisional unbalanced dataset to extract the durations
+create_dataset(new_age_group_file, input_dir, output_dir_1)
+
+# Getting information about the duration of each file
+full_duration_unbalanced = get_audio_folder_duration(output_dir_1, new_age_group_file)
 
 # Generating the file with the dataset with utterances balanced on gender
 # and age groups
@@ -42,7 +49,7 @@ print(f'\nSpeakers per gender\n{gender_speakers_df}')
 create_file_list(balanced_dataset_file, dataset_name)
 
 # Creation of the directory with the selected files
-create_dataset(balanced_dataset_file, input_dir, output_dir)
+create_dataset(balanced_dataset_file, input_dir, output_dir_2)
 
 # Getting information about the duration of each file
-full_duration = get_audio_folder_duration(output_dir, balanced_dataset_file)
+full_duration = get_audio_folder_duration(output_dir_2, balanced_dataset_file)
