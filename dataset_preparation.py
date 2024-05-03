a93ecc1f4f8fb4004fe765421af5940436da3b50
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-03 10:37:03 +0200

    Add files via upload
    Added the get_durations_from_file() function and adjusted workflow

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -5,17 +5,19 @@ information about the chosen data, such as the number of utterances per age grou
 
 from utils.dataset import DatasetPreparation
 
-dataset_name = 'MyST'
+dataset_name = 'CV_17'
+create_folder = False
 dp = DatasetPreparation(dataset_name)
     
 if 'cv' in dataset_name.lower():
 
-    all_utterances_file = 'C:/Users/Alice/validated.tsv'
-    input_dir = 'C:/Users/Alice/cv-corpus-17.0-delta-2024-03-15-en.tar/cv-corpus-17.0-delta-2024-03-15/en/clips'
+    all_utterances_file = 'cv-corpus-17.0-2024-03-15/en/validated.tsv'
+    duration_filelist = 'cv-corpus-17.0-2024-03-15/en/clip_durations.tsv'
+    input_dir = 'cv-corpus-17.0-delta-2024-03-15/en/clips'
     output_dir_1 = 'CV_17_prepared_unbalanced'
     output_dir_2 = 'CV_17_prepared_balanced'
     
-    selected_utterances = dp.select_utterances_from_cv(all_utterances_file)
+    selected_utterances = dp.select_utterances_from_cv(all_utterances_file, dataset_name)
     renamed_speakers_file = dp.rename_speaker_id(selected_utterances)       
     new_age_group_file = dp.age_group_redefinition(renamed_speakers_file)
     
@@ -29,34 +31,48 @@ if 'cv' in dataset_name.lower():
     print(f'Utterances per gender\n{gender_utterances_df}')
     print(f'\nSpeakers per gender\n{gender_speakers_df}')
     
-    # Provisional unbalanced dataset to extract the durations
-    dp.create_dataset(new_age_group_file, input_dir, output_dir_1)
+    # Getting the duration of the audio clips from the provided txt list, if present
+    if duration_filelist in locals():
+        print(f'Extracting durations from {duration_filelist}')
+        total_duration = dp.get_durations_from_file(duration_filelist, new_age_group_file)
     
-    # Getting information about the duration of each file
-    full_duration_unbalanced = dp.get_audio_folder_duration(output_dir_1, new_age_group_file)
-    
+    else:
+        try:
+           # Provisional unbalanced dataset to extract the durations
+           dp.create_dataset(new_age_group_file, input_dir, output_dir_1)
+           
+           # Getting information about the duration of each file
+           full_duration_unbalanced = dp.get_audio_folder_duration(output_dir_1, new_age_group_file)
+           
+           # Generating the file with the dataset with utterances balanced on gender
+           # and age groups
+           balanced_dataset_file = dp.select_balanced_utterances(new_age_group_file, dataset_name)
+           
+        except:
+             print('No dataset input folder provided, impossible to extract the durations from the data')
+             raise
+             
     # Generating the file with the dataset with utterances balanced on gender
-    # and age groups
+    # and age groups based on the duration of the audio clips
     balanced_dataset_file = dp.select_balanced_utterances(new_age_group_file, dataset_name)
-    
+  
     # Printing the number of speakers and utterances per age group of the balanecd dataset
     age_utterances_df, age_speakers_df = dp.count_speaker_per_group(balanced_dataset_file, 'age')
-    print(f'Utterances per age group\n{age_utterances_df}')
-    print(f'\nSpeakers per age group\n{age_speakers_df}')
+    print(f'Utterances per age group in the balanced dataset\n{age_utterances_df}')
+    print(f'\nSpeakers per age group in the balanced dataset\n{age_speakers_df}')
     
     # Printing the number of speakers and utterances per gender of the balanecd dataset
     gender_utterances_df, gender_speakers_df = dp.count_speaker_per_group(balanced_dataset_file, 'gender')
     print(f'Utterances per gender\n{gender_utterances_df}')
     print(f'\nSpeakers per gender\n{gender_speakers_df}')
-    
-    # Creation of the list of the audio files only (useful for extracting from tar/zip)
-    dp.create_file_list('new_age_group_selected_validated_sp_renamed.txt', dataset_name)
-    
-    # Creation of the directory with the selected files
-    dp.create_dataset(balanced_dataset_file, input_dir, output_dir_2)
-    
-    # Getting information about the duration of each file
-    full_duration = dp.get_audio_folder_duration(output_dir_2, balanced_dataset_file)
+      
+    if create_folder == True:
+      
+      # Creation of the list of the audio files only (useful for extracting from tar/zip)
+      dp.create_file_list(balanced_dataset_file, dataset_name)
+      
+      # Creation of the directory with the selected files
+      dp.create_dataset(balanced_dataset_file, input_dir, output_dir_2)
 
 if 'myst' in dataset_name.lower():
     
