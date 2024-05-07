ebe9073359e51abbe543d0fda112d5b14676066e
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-07 12:12:27 +0200

    Add files via upload
    Added file with function to generate the lab files

---
+++ utils/generate_lab_files.py
@@ -0,0 +1,16 @@
+import os
+from tqdm import tqdm
+
+def create_labs(main_directory, file_mapping):
+  with open(file_mapping, encoding="utf-8") as f:
+      lines = f.readlines()
+  
+  for line in tqdm(lines[1:]):
+      parts = line.strip().split("\t")
+      folder_name = parts[0]
+      audio_name = parts[1]
+      transcription = parts[2]
+      
+      lab_file_path = os.path.join(main_directory, folder_name, "{}.lab".format(os.path.splitext(audio_name)[0]))
+      with open(lab_file_path, "w", encoding="utf-8") as f1:
+          f1.write(transcription)
