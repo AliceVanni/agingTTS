4aca488ab432e8036684311aa0d7b272c7bf7803
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-08 15:02:19 +0200

    Add files via upload
    Updated the selection of the file name

--- utils/generate_lab_files.py
+++ utils/generate_lab_files.py
@@ -9,8 +9,10 @@ def create_labs(main_directory, file_mapping):
       parts = line.strip().split("\t")
       folder_name = parts[0]
       audio_name = parts[1]
+      audio_name_extension = audio_name.split('.')[-1]
+      audio_name = audio_name.replace(f'.{audio_name_extension}', '')
       transcription = parts[2]
       
-      lab_file_path = os.path.join(main_directory, folder_name, "{}.lab".format(os.path.splitext(audio_name)[0]))
+      lab_file_path = os.path.join(main_directory, folder_name, "{}.lab".format(audio_name))
       with open(lab_file_path, "w", encoding="utf-8") as f1:
           f1.write(transcription)
