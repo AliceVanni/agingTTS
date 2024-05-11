e9501beb516709527eaa8070136536e7fc3134a6
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-11 10:16:14 +0200

    Add files via upload

--- agingTTS_dataset_preparation.py
+++ agingTTS_dataset_preparation.py
@@ -7,10 +7,11 @@ from utils.agingTTS_dataset import AgingTTSdataset
 from utils.generate_lab_files import create_labs
 
 attsdp = AgingTTSdataset()
-directory_list = ['FilteredCV17', 'FilteredMyST']
+#directory_list = ['FilteredCV17', 'FilteredMyST']
+directory_list = ['FilteredMyST']
 dataframe_list = ['filtered_CV17.txt', 'filtered_myst.txt']
 
-for directory in directory_list:  
+'''for directory in directory_list:  
     
     # Choose a random file from the list and printing its sampling rate
     first_folder = os.listdir(directory)[0]
@@ -18,7 +19,7 @@ for directory in directory_list:
     file = file_list[2] if file_list[1].split('.')[-1] == 'lab' else file_list[1]
     random_file = os.path.join(directory, first_folder, file)
     _, sampling_rate = librosa.load(random_file, sr=None)
-    print(f'The sample rate for {random_file} is {sampling_rate}')
+    print(f'The sample rate for {random_file} is {sampling_rate}')'''
 
 # Creation of the directory
 #agingtts_dataframe = attsdp.create_agingtts_dataframe(dataframe_list)
