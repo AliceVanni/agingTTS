68d6fb027ee4a3a6d1f92f844c5ab5eb803f924e
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-11 11:05:37 +0200

    Delete lab_file_generation.py

--- lab_file_generation.py
+++
@@ -1,6 +0,0 @@
-from utils.generate_lab_files import create_labs
-
-main_directory = 'FilteredCV17' # Folder 
-file_mapping = 'raw_data/CV_7/gender_balanced_CV_7.txt' # Text
-
-create_labs(main_directory, file_mapping)
