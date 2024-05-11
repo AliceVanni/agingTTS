b6f06318dca6f42d8fd8fd53dd505f858b79f58f
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-11 09:45:56 +0200

    Add files via upload

--- agingTTS_dataset_preparation.py
+++ agingTTS_dataset_preparation.py
@@ -1,4 +1,5 @@
 '''Creation of the final dataset on which AgingTTS is trained'''
+
 import os
 import librosa
 
@@ -14,16 +15,18 @@ for directory in directory_list:
     # Choose a random file from the list and printing its sampling rate
     first_folder = os.listdir(directory)[0]
     file_list = os.listdir(os.path.join(directory, first_folder))
-    file = file_list[1] if file_list[0].split('.')[-1] == 'lab' else file_list[0]
+    file = file_list[2] if file_list[1].split('.')[-1] == 'lab' else file_list[1]
     random_file = os.path.join(directory, first_folder, file)
     _, sampling_rate = librosa.load(random_file, sr=None)
     print(f'The sample rate for {random_file} is {sampling_rate}')
-    
-    # Changing the audio format and rresampling at 16k Hz
-    attsdp.audio_format_to_wav(directory)
 
-agingtts_dataframe = attsdp.create_agingtts_dataframe(dataframe_list)
+# Creation of the directory
+#agingtts_dataframe = attsdp.create_agingtts_dataframe(dataframe_list)
 agingtts_directory = attsdp.create_agingtts_dataset(directory_list)
 
+# Changing the audio format and resampling at 16 kHz
+attsdp.audio_format_to_wav(agingtts_directory)
+
+'''# Create labs and age files
 create_labs(agingtts_directory, agingtts_dataframe)
-attsdp.create_age_files(agingtts_directory, agingtts_dataframe)
+attsdp.create_age_files(agingtts_directory, agingtts_dataframe)'''
