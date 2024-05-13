f742fb1e794313031e080cac627ce7e438418cce
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-13 14:31:30 +0200

    Update tools.py
    updated prediction indexes

--- utils/tools.py
+++ utils/tools.py
@@ -113,10 +113,10 @@ def expand(values, durations):
 def synth_one_sample(targets, predictions, vocoder, model_config, preprocess_config):
 
     basename = targets[0][0]
-    src_len = predictions[9][0].item()
-    mel_len = predictions[10][0].item()
+    src_len = predictions[8][0].item()
+    mel_len = predictions[9][0].item()
     mel_target = targets[7][0, :mel_len].detach().transpose(0, 1)
-    mel_prediction = predictions[2][0, :mel_len].detach().transpose(0, 1)
+    mel_prediction = predictions[1][0, :mel_len].detach().transpose(0, 1)
     duration = targets[12][0, :src_len].detach().cpu().numpy()
     if preprocess_config["preprocessing"]["pitch"]["feature"] == "phoneme_level":
         pitch = targets[10][0, :src_len].detach().cpu().numpy()
@@ -170,20 +170,20 @@ def synth_samples(targets, predictions, vocoder, model_config, preprocess_config
     basenames = targets[0]
     for i in range(len(predictions[0])):
         basename = basenames[i]
-        src_len = predictions[9][i].item()
-        mel_len = predictions[10][i].item()
-        mel_prediction = predictions[2][i, :mel_len].detach().transpose(0, 1)
+        src_len = predictions[8][i].item()
+        mel_len = predictions[9][i].item()
+        mel_prediction = predictions[1][i, :mel_len].detach().transpose(0, 1)
         duration = predictions[5][i, :src_len].detach().cpu().numpy()
         if preprocess_config["preprocessing"]["pitch"]["feature"] == "phoneme_level":
             pitch = predictions[2][i, :src_len].detach().cpu().numpy()
             pitch = expand(pitch, duration)
         else:
-            pitch = predictions[3][i, :mel_len].detach().cpu().numpy()
+            pitch = predictions[2][i, :mel_len].detach().cpu().numpy()
         if preprocess_config["preprocessing"]["energy"]["feature"] == "phoneme_level":
-            energy = predictions[4][i, :src_len].detach().cpu().numpy()
+            energy = predictions[3][i, :src_len].detach().cpu().numpy()
             energy = expand(energy, duration)
         else:
-            energy = predictions[4][i, :mel_len].detach().cpu().numpy()
+            energy = predictions[3][i, :mel_len].detach().cpu().numpy()
 
         with open(
             os.path.join(preprocess_config["path"]["preprocessed_path"], "stats.json")
@@ -204,7 +204,7 @@ def synth_samples(targets, predictions, vocoder, model_config, preprocess_config
     from .model import vocoder_infer
 
     mel_predictions = predictions[2].transpose(1, 2)
-    lengths = predictions[10] * preprocess_config["preprocessing"]["stft"]["hop_length"]
+    lengths = predictions[9] * preprocess_config["preprocessing"]["stft"]["hop_length"]
     wav_predictions = vocoder_infer(
         mel_predictions, vocoder, model_config, preprocess_config, lengths=lengths
     )
