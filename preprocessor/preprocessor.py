cf1de48d6953826624185fece84a505c103f9ddb
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-15 16:00:32 +0200

    Add files via upload

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -118,7 +118,7 @@ class Preprocessor:
         with open(os.path.join(self.out_dir, "speakers.json"), "w") as f:
             f.write(json.dumps(speakers))
         
-        age_categories = set(self.corpus_df['age'])
+        age_categories = list(set(self.corpus_df['age']))
         age_dict = {}
         for i in range(0, len(age_categories), 2):
             age_dict[age_categories[i]] = age_categories[i]
@@ -192,7 +192,8 @@ class Preprocessor:
         #with open(age_path, "r") as f:
          #   age = f.readline().strip("\n")
         # Read age information - from the DF
-        age = self.corpus_df.loc[corpus_df['client_id'] == speaker, 'age']
+        age = self.corpus_df.loc[self.corpus_df['client_id'] == speaker, 'age'].unique()
+        age = age.item(0)
         
         # Compute fundamental frequency
         pitch, t = pw.dio(
