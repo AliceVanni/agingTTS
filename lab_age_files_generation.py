8f66f528ea01f24b5c601c069f38437587ec306b
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-15 16:04:36 +0200

    Add files via upload

--- lab_age_files_generation.py
+++ lab_age_files_generation.py
@@ -1,9 +1,9 @@
 from utils.generate_lab_files import create_labs
 from utils.agingTTS_dataset import AgingTTSdataset
 
-main_directory = 'FilteredMyST' # Folder with the audio files
-file_mapping = 'filtered_myst.txt' # Text file with the dataframe
+main_directory = 'agingTTS' # Folder with the audio files
+file_mapping = 'agingTTS.txt' # Text file with the dataframe
                                       # (client_id, filename, transcription and age)
 
 create_labs(main_directory, file_mapping)
-AgingTTSdataset().create_age_files(main_directory, file_mapping)
+#AgingTTSdataset().create_age_files(main_directory, file_mapping)
