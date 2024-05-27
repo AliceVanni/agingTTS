6c9fbc9917dc86a634b84ad29b1178970ec41e7c
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2024-05-27 10:50:58 +0200

    Add files via upload

--- text/__init__.py
+++ text/__init__.py
@@ -40,6 +40,33 @@ def text_to_sequence(text, cleaner_names):
 
     return sequence
 
+def text_to_sequence_ipa(text, cleaner_names):
+    """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+
+    The text can optionally have ARPAbet sequences enclosed in curly braces embedded
+    in it. For example, "Turn left on {HH AW1 S S T AH0 N} Street."
+
+    Args:
+      text: string to convert to a sequence
+      cleaner_names: names of the cleaner functions to run the text through
+
+    Returns:
+      List of integers corresponding to the symbols in the text
+    """
+    sequence = []
+
+    # Check for curly braces and treat their contents as ARPAbet:
+    while len(text):
+        m = _curly_re.match(text)
+
+        if not m:
+            sequence += _symbols_to_sequence(_clean_text(text, cleaner_names))
+            break
+        sequence += _symbols_to_sequence(_clean_text(m.group(1), cleaner_names))
+        sequence += _ipa_to_sequence(m.group(2))
+        text = m.group(3)
+    print(f'Sequence from text.init: {sequence}')
+    return sequence
 
 def sequence_to_text(sequence):
     """Converts a sequence of IDs back to a string"""
@@ -70,6 +97,9 @@ def _symbols_to_sequence(symbols):
 def _arpabet_to_sequence(text):
     return _symbols_to_sequence(["@" + s for s in text.split()])
 
-
+def _ipa_to_sequence(text):
+    return _symbols_to_sequence(["@" + s for s in text.split()])
+    
+    
 def _should_keep_symbol(s):
-    return s in _symbol_to_id and s != "_" and s != "~"
+    return s in _symbol_to_id and s != "_" and s != "~" and s != "'"
